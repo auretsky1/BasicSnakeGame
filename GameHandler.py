38f7bddc500ca88d15997f9e68114bbf9c2b9fcb
Felix Rivera <f_rivera@knights.ucf.edu>
2015-04-09 17:32:09 -0400

    set up game_handler

---
+++ GameHandler.py
@@ -0,0 +1,25 @@
+import pygame
+
+class GameHandler(pygame.sprite.Sprite):
+
+    def __init__(self,game_screen):
+        self.game_screen = game_screen
+
+
+
+    def game_logic (self):
+        pass
+
+
+
+
+
+    def draw_game_object(self):
+        pass
+
+
+
+
+
+    def process_event(self):
+        pass
