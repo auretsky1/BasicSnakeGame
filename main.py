a3e699fb34b983fb96ac8f36d226360ab13a2295
Al Uretsky <auretsky1@yahoo.com>
2015-04-09 15:58:20 -0400

    Added boiler-plate for main game loop.

--- main.py
+++ main.py
@@ -1,2 +1,45 @@
-if __name__ == "__main__":
-    print("Snake")
+import pygame
+
+# Constants
+screen_size = (500, 500)
+screen_flags = 0
+screen_depth = 0
+
+# Color constants
+screen_clear_color = (0, 0, 0)
+
+# Variables
+is_game_running = True
+
+# Initialize PyGame
+pygame.init()
+pygame.font.init()
+
+# Setup the program window
+pygame.display.set_caption("Snake Game")
+
+# Initialize the screen
+game_screen = pygame.display.set_mode(screen_size, screen_flags, screen_depth)
+
+# Create the game clock
+game_clock = pygame.time.Clock()
+
+# Main game loop
+while is_game_running:
+    # Process events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_game_running = False
+
+    # Do game logic
+
+    # Clear the screen
+    game_screen.fill(screen_clear_color)
+
+    # Draw game objects
+
+    # Draw the screen
+    pygame.display.flip()
+
+    # Frame-rate
+    game_clock.tick(60)
